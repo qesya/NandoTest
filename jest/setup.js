579c85cc95dc285118bdccfa95884ffbba4802f3
qesya <pasquale.palena@gmail.com>
2022-03-14 19:59:14 +0700

    mock animated

--- jest/setup.js
+++ jest/setup.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler/jestSetup';
 /* eslint-disable no-undef */
 
 jest.useFakeTimers();
@@ -7,7 +8,21 @@ jest.mock('@react-navigation/native', () => {
   return {
     ...actualNav,
     useNavigation: () => ({
-      navigate: mockedNavigate,
+      navigate: jest.fn(),
+      dispatch: jest.fn(),
     }),
   };
 });
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
